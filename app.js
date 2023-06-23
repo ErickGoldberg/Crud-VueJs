aa776533febb6256b5b7d711fbbd790b55525a5a
Erick Goldberg <erick_goldberg@hotmail.com>
2023-06-23 20:43:44 -0300

    add delete

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const app = Vue.createApp({
         cadastrar(){
             let addCliente = document.querySelector("#cad_cliente").value
             this.clientes.push(addCliente)
-            
+        },
+        apagarCliente(index){
+            let clienteIndex = this.clientes.indexOf(index);
+            this.clientes.splice(clienteIndex, 1);
+        },
+        editarCliente(index){
+
         }
     }
 });
